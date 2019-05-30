a5796a966f4528de1abd78c087bf9051a6133c99
Visar7 <visar.buza7@gmail.com>
2019-05-31 00:28:14 +0200

    Test per BaseModel

--- Application/Models/BaseModel.php
+++ Application/Models/BaseModel.php
@@ -1,5 +1,27 @@
 <?php
 
 class BaseModel{
+    protected $adapter;
+    protected $table;
+    protected $modelName;
+    protected $columnNames=array();
+    public $id;
 
+    public function __construct($table){
+        $this->adapter=DB::getInstance();
+        $this->table=$table;
+        $this->setTableColumns();
+    }
+
+    protected function setTableColumns(){
+        $columns=$this->getColumns();
+        foreach ($columns as $column){
+            $this->columnNames[]=$column->Field;
+            $this->{columnName}=null;
+        }
+    }
+
+    public function getColumns(){
+        return $this->adapter->getColumns($this->table);
+    }
 }
