45939ec38729ca4b48d4ddef7668c0221c0818c0
Visar7 <visar.buza7@gmail.com>
2019-06-01 12:05:05 +0200

    isUser

--- Application/Models/EmployeesModel.php
+++ Application/Models/EmployeesModel.php
@@ -2,6 +2,17 @@
 include_once 'autoinclude.php';
 class EmployeesModel extends BaseModel{
 
+
+    public function isUser($username,$password){
+        $sql="SELECT * FROM $this->tableName WHERE username=? and password=?";
+        $param=[$username,$password];
+        $this->adapter->runQuery($sql,$param);
+        if($this->adapter->count()==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
     public function getByDepartment($deptName){
         $sql="SELECT name FROM $this->tableName NATURAL JOIN departments WHERE department_name=?";
         $param=[$deptName];
--- Application/Models/ManagersModel.php
+++ Application/Models/ManagersModel.php
@@ -3,10 +3,23 @@ include 'autoinclude.php';
 
 class ManagersModel extends BaseModel{
 
+    public function isUser($username,$password){
+        $sql="SELECT * FROM $this->tableName WHERE username=? and password=?";
+        $param=[$username,$password];
+        $this->adapter->runQuery($sql,$param);
+        if($this->adapter->count()==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public function getByDepartment($deptName){
         $sql="SELECT name FROM $this->tableName NATURAL JOIN departments WHERE department_name=?";
         $param=[$deptName];
         $this->adapter->runQuery($sql,$param);
         return $this->adapter->results();
     }
+
+
 }
