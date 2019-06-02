842b96aecda73757781b6f2af9e3da4c69233765
Visar7 <visar.buza7@gmail.com>
2019-06-02 16:56:13 +0200

    update
    Co-Authored-By: vegim-bytyqi <vegim-bytyqi@users.noreply.github.com>
    Co-Authored-By: Vigan <vigan.dika@studentet.uni-pr.edu>
    Co-Authored-By: shpatgashi <shpatgashi@users.noreply.github.com>

--- Application/autoinclude.php
+++ Application/autoinclude.php
@@ -2,5 +2,5 @@
 
 spl_autoload_register(function($className) {
     $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
-    include_once $_SERVER['DOCUMENT_ROOT'] . '/projekti/Project_Gr35_ProgramimiNeInternet/Application/' . $className . '.php';
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/PI18_19_Gr35/Application/' . $className . '.php';
 });
