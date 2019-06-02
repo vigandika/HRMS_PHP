28d76192bd6f7f77faee7b0ba6b8f2482670e5ae
Visar7 <visar.buza7@gmail.com>
2019-06-02 03:02:43 +0200

    Revert "ndryshime"
    This reverts commit 6da0b706d6c6051d38a6943887da82526ad4cee2.

--- Application/Views/employees.php
+++ Application/Views/employees.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Manager Area | Employees</title>
   <!-- Bootstrap core CSS -->
-  <link href="../Public/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../Public/css/style.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
   <script src="http://cdn.ckeditor.com/4.6.1/standard/ckeditor.js"></script>
 </head>
 
@@ -222,7 +222,7 @@
     ================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="../../Public/js/bootstrap.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
